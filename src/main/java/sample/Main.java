b8bee9991697b01a0de3feb288178f4c755513b4
daltonito <aleksandar.gjorgjevich@lunatech.com>
2025-08-22 18:01:42 +0200

    Added main window icon

--- src/main/java/sample/Main.java
+++ src/main/java/sample/Main.java
@@ -7,6 +7,7 @@ import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.GridPane;
@@ -150,10 +151,20 @@ public class Main extends Application {
 
         ReturnedRow.loadExcelData();
 
+        setWindowIcon(primaryStage);
         primaryStage.setTitle("Part number search");
         primaryStage.setResizable(false);
         primaryStage.setScene(scene);
         primaryStage.show();
+    }
+
+    private void setWindowIcon(Stage primaryStage) {
+        try {
+            Image icon = new Image(getClass().getResourceAsStream("/images/icon.ico"));
+            primaryStage.getIcons().add(icon);
+        } catch (Exception e) {
+            System.out.println("Could not load icon: " + e.getMessage());
+        }
     }
 
     public static void main(String[] args) {
